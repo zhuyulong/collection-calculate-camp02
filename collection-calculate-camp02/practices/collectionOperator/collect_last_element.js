84cc7ad46e07028f03f1f1cdfb887c3dd213605d
zhuyulong <356981944@qq.com>
2015-06-12 13:37:28 +0800

    by use portotype finash second test

--- collection-calculate-camp02/practices/collectionOperator/collect_last_element.js
+++ collection-calculate-camp02/practices/collectionOperator/collect_last_element.js
@@ -1,12 +1,10 @@
 'use strict';
-
+var _ = require('../lodash/mylodash/mylodash.js');
 function collect_last_element(collection) {
-  var _ = require('../lodash/mylodash/mylodash.js');
-
-    var num = _(collection).each(function(val){
-        return ;
-    }).value;
-    return num;
+  
+    return _(collection).reduce(function(a,b){
+        return b;
+    }).value();
 }
 
 module.exports = collect_last_element;
